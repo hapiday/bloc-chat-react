70b9beca1042254eda8e02c90ddacd2b54590598
Sara <sarakang05@gmail.com>
2018-10-31 18:48:58 -0700

    messagelist

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
+
 import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
-import RoomList from './components/RoomList';
+import { RoomList } from './components/RoomList.js';
+import { MessageList } from './components/MessageList.js';
 
-// Initialize Firebase
- var config = {
-   apiKey: "AIzaSyAkUx01__RRp0RZW-Ml5AkYILsXnTdmgdY",
-   authDomain: "bloc-chat-de04f.firebaseapp.com",
-   databaseURL: "https://bloc-chat-de04f.firebaseio.com",
-   projectId: "bloc-chat-de04f",
-   storageBucket: "bloc-chat-de04f.appspot.com",
-   messagingSenderId: "593644873907"
- };
- firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyAkUx01__RRp0RZW-Ml5AkYILsXnTdmgdY",
+    authDomain: "bloc-chat-de04f.firebaseapp.com",
+    databaseURL: "https://bloc-chat-de04f.firebaseio.com",
+    projectId: "bloc-chat-de04f",
+    storageBucket: "bloc-chat-de04f.appspot.com",
+    messagingSenderId: "593644873907"
+  };
+  firebase.initializeApp(config);
 
- class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <RoomList firebase={firebase} />
-      </div>
-    );
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {activeRoom: ""};
+      this.activeRoom = this.activeRoom.bind(this);
+    }
+   activeRoom(room) {
+    this.setState({ activeRoom: room })
+  }
+     render() {
+      const showMessages = this.state.activeRoom;
+      return (
+        <div>
+          <h1>{this.state.activeRoom.title || "ROOMS"}</h1>
+          <RoomList firebase={firebase} activeRoom={this.activeRoom} />
+          <MessageList firebase={firebase} activeRoom={this.state.activeRoom.key} />
+        </div>
+      );
+    }
   }
 
-}
-
-export default App;
+  export default App;
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,16 +1,76 @@
 import React, { Component } from 'react';
+import '../App.css';
+//
+// export class RoomList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       rooms: [],
+//       newRoomName: ""
+//     };
+//
+//     this.roomsRef = this.props.firebase.database().ref('rooms');
+//     this.createRoom = this.createRoom.bind(this);
+//     this.nameChange = this.nameChange.bind(this);
+//     //
+//   }
+//   // this is doing this
+//   componentDidMount() {
+//     this.roomsRef.on('child_added', snapshot => {
+//     const room = snapshot.val();
+//     room.key = snapshot.key;
+//     this.setState({ rooms: this.state.rooms.concat(room) })
+//     });
+//   }
+//   //this is doin this
+//   createRoom(e) {
+//     e.preventDefault()
+//     this.roomsRef.push({ name: this.state.newRoomName });
+//     this.setState({ newRoomName: ""});
+//   }
+//   nameChange(e) {
+//     this.setState({newRoomName : e.target.value});
+//   }
+//
+//   selectRoom(room) {
+//     this.props.activeRoom(room);
+// }
+//     render() {
+//     let room = (
+//       <form onSubmit={this.createRoom}>
+//         <input type="text" value={this.state.newRoomName} placeholder="New Room Name" onChange={this.nameChange}/>
+//         <input type="submit" value="New Chat" />
+//       </form>
+//       );
+// //
+//
+// let roomList = this.state.rooms.map((room) =>
+//     <li key={room.key} onClick={(e) => this.selectRoom(room, e)}>{room.title} </li>
+// );
+//
+//     return(
+//       <div>
+//         <div>{room}</div>
+//         <ul>{roomList}</ul>
+//       </div>
+//     );
+//    }
+// }
+
+// setting a variable called roomList that will iterate through room by using a method called ".map"
+// and it will grab a room's key and name value and return them as lists
+
+// export default RoomList;
+//
+// import React, { Component } from 'react';
 
 export class RoomList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      rooms: [],
-      newRoomName: ""
-    };
-
-    this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.state = {title: "", rooms: []};
+    this.roomsRef = this.props.firebase.database().ref("rooms");
+    this.handleChange = this.handleChange.bind(this);
     this.createRoom = this.createRoom.bind(this);
-    this.nameChange = this.nameChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,38 +78,42 @@ export class RoomList extends Component {
       const room = snapshot.val();
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat(room) })
-    });
-  }
-  createRoom(e) {
-    e.preventDefault()
-    this.roomsRef.push({ name: this.state.newRoomName });
-    this.setState({ newRoomName: ""});
-  }
-  nameChange(e) {
-    this.setState({newRoomName : e.target.value});
-  }
-
-    render() {
-        let room = (
-        <form onSubmit={this.createRoom}>
-          <input type="text" value={this.state.newRoomName} placeholder="New Room Name" onChange={this.nameChange}/>
-          <input type="submit" value="New Chat" />
-        </form>
-      );
-
-       let roomList = this.state.rooms.map((room) =>
-        <li key={room.key}>{room.name}</li>
-       );
-
-      return(
-        <div>
-          <div>{room}</div>
-          <ul>{roomList}</ul>
-        </div>
-      )
-  }
+    }
+  );
+}
+handleChange(e) {
+  this.setState({ title: e.target.value });
 }
 
+createRoom(e) {
+  e.preventDefault();
+  this.roomsRef.push({ title: this.state.title });
+  this.setState({ title: "" });
+}
 
+selectRoom(room) {
+  this.props.activeRoom(room);
+}
 
+render() {
+  const roomForm = (
+    <form onSubmit={this.createRoom}>
+    <input type="text" value={this.state.title} placeholder="Enter Room Name" onChange={this.handleChange}/>
+    <input type="submit" value="Create" />
+    </form>
+  );
+
+  const roomList = this.state.rooms.map((room) =>
+  <li key={room.key} onClick={(e) => this.selectRoom(room, e)}>{room.title}</li>
+);
+
+return(
+  <div>
+  <div>{roomForm}</div>
+  <ul>{roomList}</ul>
+  </div>
+  
+    );
+  }
+}
 export default RoomList;
